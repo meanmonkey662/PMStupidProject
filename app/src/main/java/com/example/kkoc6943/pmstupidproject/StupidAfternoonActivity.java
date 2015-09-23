2d4e52dd4de3d548c0595c756214ee2281ae89e3
meanmonkey662 <kkoc6943@740s-pl-132.local>
2015-09-23 13:54:15 -0600

    added change

--- app/src/main/java/com/example/kkoc6943/pmstupidproject/StupidAfternoonActivity.java
+++ app/src/main/java/com/example/kkoc6943/pmstupidproject/StupidAfternoonActivity.java
@@ -1,11 +1,12 @@
 package com.example.kkoc6943.pmstupidproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class StupidAfternoonActivity extends AppCompatActivity {
+public class StupidAfternoonActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
