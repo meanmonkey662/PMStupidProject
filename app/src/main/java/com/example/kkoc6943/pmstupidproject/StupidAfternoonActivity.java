a5959c83d88126c60f2ff8a4cc71cbb5de591130
meanmonkey662 <kkoc6943@740s-pl-132.local>
2015-09-25 13:11:47 -0600

    changes colors!

--- app/src/main/java/com/example/kkoc6943/pmstupidproject/StupidAfternoonActivity.java
+++ app/src/main/java/com/example/kkoc6943/pmstupidproject/StupidAfternoonActivity.java
@@ -5,24 +5,44 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.RelativeLayout;
+import android.view.View;
+import android.graphics.Color;
+import android.graphics.Color;
 
-public class StupidAfternoonActivity extends ActionBarActivity {
+public class StupidAfternoonActivity extends ActionBarActivity
+{
+    private Button colorChangeButton;
+    private RelativeLayout background;
+    private TextView sillyWords;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stupid_afternoon);
-    }
+
+        colorChangeButton = (Button) findViewById(R.id.sillyButton);
+        background = (RelativeLayout) findViewById(R.id.appBackground);
+        sillyWords = (TextView) findViewById(R.id.sillyWords);
+
+        setupListeners();
+     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_stupid_afternoon, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -34,5 +54,37 @@ public class StupidAfternoonActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void changeColors()
+    {
+        int redColor;
+        int greenColor;
+        int blueColor;
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        background.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+        redColor = (int) (Math.random() * 256);
+        greenColor = (int) (Math.random() * 256);
+        blueColor = (int) (Math.random() * 256);
+
+        colorChangeButton.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+    }
+
+    public void setupListeners()
+    {
+        colorChangeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View buttonView)
+            {
+                //This is where you put code that happens when you click a button.
+                changeColors();
+            }
+        });
     }
 }
